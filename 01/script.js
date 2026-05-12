77165357c23242b7a568bea42d95bd9d04ef8fae
dondoco127 <mr.000.gim@gmail.com>
2026-05-12 11:44:08 +0900

    Add files via upload

---
+++ 01/script.js
@@ -0,0 +1,4 @@
+document.write('Hello World!!');  //document.write()は()内に記述した文字列をbody要素の末尾に表示
+document.querySelector('p').textContent = 'ohla!!';  //p要素のコンテント（「ここを書き換える」という文字列）をohla!!で上書きする
+// window.alert('アラート表示');  //実行すると警告ウィンドウとして降りてきてOKを押すと消える
+console.log('見つかった');  //ブラウザーの検証→コンソールに表示される
